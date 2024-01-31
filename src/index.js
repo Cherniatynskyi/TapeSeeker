3411ed279afb1d673016b82f495428ccc47122bb
Mark Cherniatynskyi <markushalot34@gmail.com>
2024-01-31 19:04:51 +0200

    Chnaged repo name

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {BrowserRouter} from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <BrowserRouter basename='/goit-react-hw-05-movies'>
+    <BrowserRouter basename='/TapeSeeker'>
       <App />
     </BrowserRouter>
   // </React.StrictMode>
